c8ecf1090b82abc06efbca7dace8bbe40efeaa20
Nathan-CSE <86046189+Nathan-CSE@users.noreply.github.com>
2024-02-21 23:14:39 +1100

    tried debugging rust

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -1,6 +1,6 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
-use serde_json;
+use serde_json::{self, json};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
@@ -10,17 +10,19 @@ pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse
     // - We're given a struct type 'DataRequest' that is able to be deserialized -> we need to deserialize it,
     // calcualte the adequate sums and then send it back
 
-
+    // Can't seem to test with POSTMAN -> Failed to deserialize the JSON body into the target type: data[0]: unknown variant `Hello`, expected `String` or `Number` at line 2 column 18
+    // Seems that the server is receiving the data, but the program can't deserialize it
+    // Not sure why -> the function doesn't even run
 
     let mut string_len: i32 = 0;
     let mut int_sum: i32 = 0;
 
-    println!("dolasihdsoak");
     
     for item in request.data {
         match item {
             Data::String(x) => string_len += x.chars().count() as i32,
             Data::Number(x) => int_sum += x,
+
         };
     };
 
@@ -39,7 +41,7 @@ pub struct DataRequest {
     pub data: Vec<Data>
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, deser)]
 pub struct DataResponse {
     // Add any fields here
     pub string_len: i32,
--- chaos/src/main.rs
+++ chaos/src/main.rs
@@ -6,6 +6,8 @@ mod data;
 async fn main() {
     let app = Router::new().route("/process_data", post(data::process_data));
 
+    println!("ASLIDHJASLDJK");
+
     axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
